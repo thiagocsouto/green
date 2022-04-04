c90f681c9a964092c193583c78ca4cea9d044f73
Thiago Souto <thiagocsouto@outlook.com>
2022-04-04 08:40:30 -0400

    Delete green/green directory

--- green/green/src/main/java/com/eco/green/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eco.green.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eco.green.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
--- green/green/target/classes/data-h2.sql
+++
@@ -1,3 +0,0 @@
-insert into user_green(id, name, age, sex, email, suggestion) values
-(1, 'Willian', 21, 'MASCULINE', 'CSwill@outlook.com', 'very good, I like the environment')
-;
