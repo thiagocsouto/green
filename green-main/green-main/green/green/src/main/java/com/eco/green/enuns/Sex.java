6bb9f4fdc752f72ca6f1520ee71c8a9f9b4c00dc
Thiago Souto <thiagocsouto@outlook.com>
2022-04-04 08:43:38 -0400

    Add files via upload

---
+++ green-main/green-main/green/green/src/main/java/com/eco/green/enuns/Sex.java
@@ -0,0 +1,16 @@
+package com.eco.green.enuns;
+
+public enum Sex {
+		
+		MASCULINE("masculine"),
+		FEMININE("feminine"),
+		OTHER("other");
+		
+		private String Sex; 
+
+		private Sex(String sex) {
+			Sex = sex;
+		} 
+
+
+}
