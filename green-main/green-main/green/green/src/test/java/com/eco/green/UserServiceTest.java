46cc4d08556b3071ec90e879362047316448af16
Thiago Souto <thiagocsouto@outlook.com>
2022-05-19 10:54:03 -0400

    Update UserServiceTest.java

--- green-main/green-main/green/green/src/test/java/com/eco/green/UserServiceTest.java
+++ green-main/green-main/green/green/src/test/java/com/eco/green/UserServiceTest.java
@@ -20,19 +20,18 @@ import com.eco.green.enuns.Sex;
 import com.eco.green.repository.UserRepository;
 import com.eco.green.service.UserService;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@SpringBootTest
 class UserServiceTest { 
 	
 	@Autowired
 	private UserRepository userRepository;
 	 
-//	@Autowired
-//	private UserService userService;
+	@Autowired
+	private UserService userService;
 
 	@Test
       void testeParaVerficarCadastroDeFuncionariosEstaCorreto() {
-		User user = new User("Thiago", 21 , Sex.MAN, "THIAGO@OUTLOOK.COM", "I LIKED"); 
+		User user = new User("Taison", 33 , Sex.MAN, "INTER@OUTLOOK.COM", "I LIKED GREEN"); 
 		User savedUser = userRepository.save(user); 	
 		
 		assertNotNull(savedUser);
@@ -41,7 +40,6 @@ class UserServiceTest {
 	
 	@Test
 	 void testCadastrandoUsuarioComIdadeIgualOuMenorAZero() throws Exception {
-        UserService userService = new UserService();
         
 		assertThrows(IllegalArgumentException.class,
 				() -> userService.salvarUser(new User("Thiago", 0 , Sex.MAN, "THIAGO@OUTLOOK.COM", "I LIKED")));
@@ -49,7 +47,6 @@ class UserServiceTest {
 	
 	@Test
 	 void testCadastrandoEmailUsuarioNulo() throws Exception {
-       UserService userService = new UserService();
        User user = new User("Thiago", 21 , Sex.MAN, null, "I LIKED");
        Exception exception = assertThrows(Exception.class, () -> userService.salvarUser(user));
    
@@ -59,3 +56,4 @@ class UserServiceTest {
 	
 	
 }
+
