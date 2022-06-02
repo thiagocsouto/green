d40fcf894d5884c7c3ec4d3b6ae25c29281efdf2
Thiago Souto <thiagocsouto@outlook.com>
2022-06-02 13:50:01 -0400

    Delete green/green directory

--- green/green/target/classes/data-h2.sql
+++
@@ -1,7 +0,0 @@
-insert into user_green(id, name, age, sex, email, suggestion) values
-(1, 'Willian', 27, 'MAN', 'CSwill@outlook.com', 'very good, I like the environment'),
-(2, 'Gustavo', 20, 'MAN', 'gustavo@outlook.com', 'very well, I liked'),
-(3, 'Thiago', 21, 'MAN', 'thiagoantoniocs47@gmail.com', 'Oh, Good')
-;
-
-	
