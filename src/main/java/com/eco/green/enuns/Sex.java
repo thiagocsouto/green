7ea9dd2dfde51cf66496237e815c326534b9eba0
p001284 <julimar@prodam.am.gov.br>
2022-08-09 14:05:29 -0400

    Commit Inicial

---
+++ src/main/java/com/eco/green/enuns/Sex.java
@@ -0,0 +1,18 @@
+package com.eco.green.enuns;
+
+public enum Sex {
+		
+		MAN("man"),
+		WOMAN("woman"),
+		OTHER("other");
+		
+		private String Sex; 
+
+		private Sex(String sex) {
+			Sex = sex;
+		}
+
+
+		
+
+}
---
+++ src/main/java/com/eco/green/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.eco.green.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eco.green.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	
+	User findByEmail( String email);
+
+}
